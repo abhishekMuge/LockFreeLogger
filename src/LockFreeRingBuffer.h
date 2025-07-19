775fc11e97e4527c3b47e0fc95f9f0092894f399
abhishekmuge <muge111hhh@gmail.com>
2025-07-19 14:14:54 +0530

    updated enque method to hand MPSC case

--- src/LockFreeRingBuffer.h
+++ src/LockFreeRingBuffer.h
@@ -9,7 +9,28 @@ public:
     LockFreeRingBuffer() : head_(0), tail_(0) {}
 
     bool enqueue(const LogEntry& entry) {
-        size_t current_head = head_.load(std::memory_order_relaxed);
+    	size_t current_head;
+
+    	while(true){
+    		current_head = head_.load(std::memory_order_relaxed);
+    		size_t tail = tail_.load(std::memory_order_acquire);
+
+    		size_t next_head = (current_head + 1) & (SIZE - 1); // replaced % SIZE
+
+    		if (next_head == tail) {
+				return false; // Buffer is full
+			}
+
+    		if (head_.compare_exchange_weak(current_head, next_head,
+											std::memory_order_acquire,
+											std::memory_order_relaxed)) {
+				buffer_[current_head] = entry;
+				return true;
+			}
+    	}
+    	//Below changes is done to handle the Multi Producer and Single Consumer thread
+    	//in cases of multple producer is trying to add data in same index, will be loss of data
+        /*size_t current_head = head_.load(std::memory_order_relaxed);
         size_t next_head = (current_head + 1) % SIZE;
 
         if (next_head == tail_.load(std::memory_order_acquire)) {
@@ -18,7 +39,7 @@ public:
 
         buffer_[current_head] = entry;
         head_.store(next_head, std::memory_order_release);
-        return true;
+        return true;*/
     }
 
     std::optional<LogEntry> dequeue() {
